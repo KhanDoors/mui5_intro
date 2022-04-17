5d7d5c7e9683a5cce92b8ebcba97b6b57f93583e
syyad <syyadkhan@yahoo.com>
2022-04-17 12:17:44 -0400

    minute 55

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,17 @@
 import { Box } from "@mui/material";
 import React from "react";
 
+import Posts from "./Posts";
+
 const Feed = () => {
   return (
-    <Box bgcolor="blue" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      <Posts />
+      <Posts />
+      <Posts />
+      <Posts />
+      <Posts />
+      <Posts />
     </Box>
   );
 };
--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,207 @@
-import { Box } from "@mui/material";
+import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
 import React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+  },
+];
 
 const Rightbar = () => {
   return (
-    <Box
-      bgcolor="orange"
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      Rightbar
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed" width={300}>
+        <Typography variant="h4" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt="Cindy Baker"
+            src="https://images.pexels.com/photos/620074/pexels-photo-620074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://images.pexels.com/photos/89779/girl-rustic-grass-meadow-89779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://images.pexels.com/photos/3283568/pexels-photo-3283568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+          <Avatar
+            alt="Remy Sharp"
+            src="https://images.pexels.com/photos/3715196/pexels-photo-3715196.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.pexels.com/photos/458766/pexels-photo-458766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />{" "}
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.pexels.com/photos/3356130/pexels-photo-3356130.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />{" "}
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.pexels.com/photos/159780/colors-hairdresser-cutting-colorimetry-159780.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />{" "}
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.pexels.com/photos/3522179/pexels-photo-3522179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          />
+        </AvatarGroup>
+        <Typography variant="h4" fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList
+          sx={{ width: 500, height: 450 }}
+          cols={3}
+          rowHeight={164}
+          gap={5}
+        >
+          {itemData.map((item) => (
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <Typography variant="h4" fontWeight={100} mt={2}>
+          Latest Coversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://images.pexels.com/photos/620074/pexels-photo-620074.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Brittany Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Travis Howard"
+                src="https://images.pexels.com/photos/89779/girl-rustic-grass-meadow-89779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Stacy, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Cindy Baker"
+                src="https://images.pexels.com/photos/3283568/pexels-photo-3283568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
